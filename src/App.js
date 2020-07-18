35b139b9e941dbd7b556eb709ef43c558c92426d
numankh <khannuman15@gmail.com>
2020-07-18 08:38:35 -0400

    First commit

--- src/App.js
+++ src/App.js
@@ -1,56 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import "./App.css";
+import allActions from "./actions";
 
 function App() {
+  const counter = useSelector((state) => state.counter);
+  const currentUser = useSelector((state) => state.currentUser);
+
+  const dispatch = useDispatch();
+
+  const user = { name: "Rei" };
+
+  useEffect(() => {
+    dispatch(allActions.userActions.setUser(user));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
+      {currentUser.loggedIn ? (
+        <>
+          <h1>Hello, {currentUser.user.name}</h1>
+          <button onClick={() => dispatch(allActions.userActions.logOut())}>
+            Logout
+          </button>
+        </>
+      ) : (
+        <>
+          <h1>Login</h1>
+          <button
+            onClick={() => dispatch(allActions.userActions.setUser(user))}
           >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+            Login
+          </button>
+        </>
+      )}
+
+      <h1>Counter: {counter}</h1>
+      <button onClick={() => dispatch(allActions.counterActions.increment())}>
+        Increase Counter
+      </button>
+      <button onClick={() => dispatch(allActions.counterActions.decrement())}>
+        Decrease Counter
+      </button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,21 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import store from './app/store';
-import { Provider } from 'react-redux';
-import * as serviceWorker from './serviceWorker';
+import { createStore } from "redux";
+import rootReducer from "./reducers";
+import { Provider } from "react-redux";
+import ReactDOM from "react-dom";
+import React from "react";
+import App from "./App";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
